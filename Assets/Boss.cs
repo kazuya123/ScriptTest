7f896ce92ce44d6ff1d3416eeb77eea314d5069e
kazuya <ikazuya123@hotmail.co.jp>
2017-11-27 22:44:30 +0900

    ScriptTestプロジェクト 初コミット

---
+++ Assets/Boss.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss
+{
+    public void Magic(int mp)
+    {
+        if (mp >= 10)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+            
+        }
+            else {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+        
+    }
+        // Use this for initialization
+        void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    int mp = 53;
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = { 30, 20, 50, 10, 80 };
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for(int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+        Boss boss = new Boss();
+       
+        for (int i = 0; i < 10; i++)
+        {
+            boss.Magic(mp);
+            mp -= 10;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+
